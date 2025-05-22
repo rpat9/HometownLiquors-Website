4ca2a3768a9a1fd16e442b20f2baa2ef9a3c616d
rpat9 <rudrap26176@gmail.com>
2025-05-21 19:59:47 -0500

    Added private routes for security

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ProductDetail from "./pages/ProductDetail";
 import Cart from "./pages/Cart";
 import Admin from "./pages/Admin";
 import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -16,19 +17,23 @@ function App() {
         <Navbar />
         <div className="pt-20">
           <Routes>
+
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/products" element={<Products />} />
             <Route path="/products/:id" element={<ProductDetail />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/admin" element={<Admin />} />
+
+            
+            <Route path="/cart" element={<PrivateRoute> <Cart /></PrivateRoute>} />
+            <Route path="/dashboard" element={<PrivateRoute> <Dashboard /></PrivateRoute>} />
+            <Route path="/admin" element={<PrivateRoute> <Admin /></PrivateRoute>} />
+            
           </Routes>
         </div>
       </div>
     </Router>
-  )
+  );
 }
 
 export default App;
